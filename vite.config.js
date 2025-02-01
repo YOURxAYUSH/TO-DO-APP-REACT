96186bddf9d730c5de5191c24efe1be6cda84a1e
YOURxAYUSH <vayuayu9568@gmail.com>
2025-02-01 20:29:48 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
   plugins: [react(),
     tailwindcss(),
   ],
+  base: '/TO-DO-APP-REACT/',
+
 })
